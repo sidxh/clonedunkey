e1b575d3c61fa58758561cbe5dda8e0ade0f2530
sidxh <totallysiddhant@gmail.com>
2023-11-15 02:00:37 +0530

    linting errors

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,7 +13,7 @@ const Header: React.FC = () => {
             <div className="flex items-center justify-between">
               <Link aria-label="Home" href="/"><UnikeySVG /> </Link>
               <div className="flex items-center gap-x-2 md:gap-x-8">
-                <Link className="whitespace-nowrap inline-flex rounded-full px-4 py-1.5 text-sm font-semibold transition bg-gray-950 text-white hover:bg-gray-800" href="/careers"><span className="relative top-px">We're hiring</span></Link>
+                <Link className="whitespace-nowrap inline-flex rounded-full px-4 py-1.5 text-sm font-semibold transition bg-gray-950 text-white hover:bg-gray-800" href="/careers"><span className="relative top-px">We&apos;re hiring</span></Link>
                 <Link className="inline-flex rounded-full px-4 py-1.5 text-sm font-semibold transition bg-gray-950 text-white hover:bg-gray-800" href="/app"><span className="relative top-px">Dashboard</span></Link>
                 <button type="button" aria-expanded="false" aria-controls=":R6mla:" className="group -m-2.5 rounded-full p-2.5 transition hover:bg-gray-950/10" aria-label="Toggle navigation"><ToggleSVG /></button>
               </div>
